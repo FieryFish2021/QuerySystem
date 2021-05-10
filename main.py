1041291839bc955647c12f46bfbcb0cb6d09a6b1
FieryFish2021 <>
2021-05-10 21:15:55 +0000

    Store the questions and login credentials with repl database

--- main.py
+++ main.py
@@ -12,16 +12,40 @@ def index():
 # Reference:
 # https://pythonspot.com/login-authentication-with-flask/
 
-# We need to store the password and email in our database.
-# Since we don't have to worry about security,
+# Store the password and email in our database.
+# Don't worry about security.
 # a dictionary might do the trick.
 
 # The repl database is already used to store the questions,
 # so it might be trickier to store pw/emails in the repl database.
 
+# Calling this function clears all keys in the repl database
+def clear_database():
+  for key in db.keys():
+    del db[key]
+  # INITIALIZE the databases
+  db['DATABASE_QUESTIONS']=[]
+  db['DATABASE_LOGIN_CREDENTIALS']={}
+
+#clear_database()
+
+print(db["DATABASE_QUESTIONS"])
+print(db["DATABASE_LOGIN_CREDENTIALS"])
+
 @web_site.route('/signup',methods=['POST','GET'])
 def signup():
   return render_template('signup.html')
+
+
+
+@web_site.route('/create_new_account',methods=['POST','GET'])
+def create_new_account():
+  password = request.form['password']
+  email = request.form['email']
+  db["DATABASE_LOGIN_CREDENTIALS"][email]=password
+  return '%s Account Created! <br/> <a href="/">Back Home</a>' % (db.keys())
+#return render_template('signup.html')
+
 
 @web_site.route('/login',methods=['POST','GET'])
 def login():
@@ -29,19 +53,10 @@ def login():
   
 
 # ------------ Log a Question ------------
-@web_site.route('/question', methods=['POST'])
+@web_site.route('/submit_question', methods=['POST'])
 def question():
 	name = request.form['name']
 	question = request.form['question']
-	
-	
-	# Commented out because this method is too primitive.
-	'''
-	#f = open('questions.log', 'a')
-	#f.write(question + ':' + name)
-	#f.write('\n')
-	#f.close()
-	'''
 	
 	# Log the quesiton and the name to the repl database.
 	# TODO: Log other infomation as well:
@@ -54,7 +69,7 @@ def question():
 	# and the list will be the VALUE.
 	# db[question] = [[name1,name2],date_time,resolved]
 	
-	db[question] = name
+	db['DATABASE_QUESTIONS'].append([name,question])
 	for key in db.keys():
 	   print(db[key])
 	return '%s Question Submitted <br/> <a href="/">Back Home</a>' % (db.keys())
@@ -63,41 +78,34 @@ def question():
 # ------------ Display a Single Question ------------
 @web_site.route('/answer', methods=['POST','GET'])
 def answer():
-  if len(list(db.keys()))==0:
+  if len(db['DATABASE_QUESTIONS'])==0:
     return render_template('no_questions.html')
-  question = list(db.keys())[0]
-  print(question)
-  question_data = db[question]
+  question = db['DATABASE_QUESTIONS'][0][1]
+  print(['DATABASE_QUESTIONS'][0])
+  question_data = db['DATABASE_QUESTIONS'][0][0]
   return render_template('answer.html',data=question_data,question=question)
 
 
 
 # ------------ Display the Next Question ------------
-@web_site.route('/next_question')
+@web_site.route('/resolve_question')
 def next_question():
   
-  question = list(db.keys())[0]
+  print(db['DATABASE_QUESTIONS'][0])
   # DELETE the previous question
   # TODO: find a way to ARCHIVE the resolved questions,
   #       instead of DELETING them
-  del db[question]
-  
+  del db['DATABASE_QUESTIONS'][0]
   return answer()
 
 # ------------ Display All Questions ------------
 @web_site.route('/all_questions', methods=['GET'])
 def all_questions():
-	return render_template('all_questions.html',keys=db.keys())
+	return render_template('all_questions.html',keys=db['DATABASE_QUESTIONS'])
 
-
-'''
-def clear_database():
-  for key in db.keys():
-    del db[key]
-
-'''
 
 if __name__ == '__main__':
 	web_site.run(host='0.0.0.0', port=3001)
 
 # Hello World
+# Hello there!
